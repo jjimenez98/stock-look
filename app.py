5a3bae9f965a861dba01528eb827bb7cd977894e
Javier Jimenez <javierjimenez@Javiers-MacBook-Pro.local>
2020-08-19 01:07:39 -0700

    added in development message to home page

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ from datetime import datetime
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
+app.config['SECRET_KEY']= 'fdsfdasfdsafrqgopmtbombbgrer'
 db = SQLAlchemy(app)
 
 class Account(db.Model):
