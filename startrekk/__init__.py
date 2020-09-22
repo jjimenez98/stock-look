0a37ebf1774a70c10612a66028fc1b581269c0be
Javier Jimenez <javierjimenez@Javiers-MacBook-Pro.local>
2020-09-22 15:46:50 -0700

    make it better

--- startrekk/__init__.py
+++ startrekk/__init__.py
@@ -6,5 +6,7 @@ app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
 app.config['SECRET_KEY']= 'fdsfdasfdsafrqgopmtbombbgrer'
 db = SQLAlchemy(app)
+app.logger.addHandler(logging.StreamHandler(sys.stdout))
+app.logger.setLevel(logging.ERROR)
 
 from startrekk import routes
